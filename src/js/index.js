350272ef88601df641c98a9a77b8d052318fced0
Ben Allred <skuttleman@gmail.com>
2018-06-10 21:24:19 -0600

    Copy over static assets

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,28 @@ require('font-awesome/css/font-awesome.css');
 
 // Require static assets
 require('../html/index.html');
-require('../../static/images/snow.jpg');
 require('../scss/main.scss');
+
+require('../../static/json/albums.json');
+require('../../static/json/apps.json');
+require('../../static/json/bios.json');
+require('../../static/json/header.json');
+require('../../static/json/songs.json');
+
+require('../../static/images/climbing.jpg');
+require('../../static/images/hiking.jpg');
+require('../../static/images/music.jpg');
+require('../../static/images/portfolio.jpg');
+require('../../static/images/snow.jpg');
+require('../../static/images/view.jpg');
+
+require('../../static/music/itunes.png');
+require('../../static/music/LetsPutThePastBackTogether.mp3');
+require('../../static/music/MorningAfterPill.mp3');
+require('../../static/music/slave-to-time.jpg');
+require('../../static/music/story-of-my-life.jpg');
+require('../../static/music/SwornToSecrecy.mp3');
+require('../../static/music/TheNextStep.mp3');
 
 var Elm = require('../elm/Main.elm');
 var mountNode = document.getElementById('app');
